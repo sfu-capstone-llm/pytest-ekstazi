7a51e0c5d5b4239620772d86c350f162fe18af34
Marcus Chan <marcuscchan@gmail.com>
2024-10-17 18:51:15 +0000

    more stuff

--- scalpel_playgroud.py
+++ scalpel_playgroud.py
@@ -5,30 +5,37 @@ from typing import Deque
 
 from scalpel import cfg, import_graph
 
+src = textwrap.dedent(
+    """
+def f():
+    x = 1
+    try:
+        a = open("a.txt", "r")
+        x = 2
+        a.close()
+    except IOError:
+        x = 3
+    except:
+        x = 4
+    else:
+        x = 5
+    finally:
+        y = 1
+    
+    y += 6
+    return x + y    
+"""
+)
+
 
 def foo():
-    src = textwrap.dedent(
-        """
-    def f():
-        x = 1
-        try:
-            a = open("a.txt", "r")
-            x = 2
-            a.close()
-        except IOError:
-            x = 3
-        except:
-            x = 4
-        else:
-            x = 5
-        finally:
-            y = 1
-        
-        y += 6
-        return x + y    
-    """
-    )
+    c = cfg.CFGBuilder().build_from_src(name="top_level", src=src)
+    assert c is not None
+    for (_, _), fun_cfg in c.functioncfgs.items():
+        print("")
 
+
+def stuff():
     p = ast.parse(src)
     print(f"p.body {p.body}")
     ast_f = ast.parse(src).body[0]
@@ -36,6 +43,7 @@ def foo():
 
     # ast_f.
     queue: Deque[ast.AST] = deque()
+    queue.append(ast_f)
 
     while len(queue) > 0:
         node = queue.popleft()
@@ -43,8 +51,7 @@ def foo():
         children = ast.iter_child_nodes(node)
         for child in children:
             queue.append(child)
-
-        print(f"{node.__str__}")
+            print(f"{node.__str__}->{child.__str__}")
 
 
 if __name__ == "__main__":
