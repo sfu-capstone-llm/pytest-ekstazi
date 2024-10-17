067e5a717ddff5266f78e73438d23e8027b1af41
Marcus Chan <marcuscchan@gmail.com>
2024-10-17 18:25:13 +0000

    sstuffs

--- scalpel_playgroud.py
+++ scalpel_playgroud.py
@@ -1,5 +1,7 @@
 import ast
 import textwrap
+from collections import deque
+from typing import Deque
 
 from scalpel import cfg, import_graph
 
@@ -28,9 +30,21 @@ def foo():
     )
 
     p = ast.parse(src)
-    print(p.body)
+    print(f"p.body {p.body}")
     ast_f = ast.parse(src).body[0]
-    print(ast_f._attributes)
+    print(f"attributes {ast_f._attributes}")
+
+    # ast_f.
+    queue: Deque[ast.AST] = deque()
+
+    while len(queue) > 0:
+        node = queue.popleft()
+
+        children = ast.iter_child_nodes(node)
+        for child in children:
+            queue.append(child)
+
+        print(f"{node.__str__}")
 
 
 if __name__ == "__main__":
