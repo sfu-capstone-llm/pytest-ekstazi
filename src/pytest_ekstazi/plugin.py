4fcd93af731c2486782bd55228288aff4571c20e
Brian Rahadi <brian.rahadi@gmail.com>
2024-11-07 08:58:44 -0800

    rerun impl

--- src/pytest_ekstazi/plugin.py
+++ src/pytest_ekstazi/plugin.py
@@ -4,6 +4,7 @@ from dataclasses import asdict, dataclass
 from hashlib import md5
 from types import FrameType
 from typing import Dict, List
+import collections
 import logging
 logger = logging.getLogger(__name__)
 
@@ -33,32 +34,35 @@ def bar(request):
 class TestDependency:
     src: str
     hash: str
-
-
-deps: Dict[str, List[TestDependency]] = {}
+    
+json_deps = {}
+deps: Dict[str, List[TestDependency]] = collections.defaultdict(list)
+newDeps: Dict[str, List[TestDependency]] = collections.defaultdict(list)
 parent = ""
+
+def should_run_file(filename: str) -> bool:
+    dirs = filename.split("/")
+    excluded_dirs = {".tox", ".venv", ".local"}
+
+    if not dirs[-1].endswith(".py"):
+        return False
+
+    if any(dir in excluded_dirs for dir in dirs):
+        return False
+    
+    return True
 
 
 def run_all_handler(frame: FrameType, event: str, _):
     if event != "call":
         return
-    global parent
 
     filename = frame.f_code.co_filename
-    key = parent if parent != filename else filename
-
-    dirs = filename.split("/")
-    excluded_dir = {".tox", ".venv", ".local"}
-
-    if not dirs[-1].endswith(".py"):
+    if not should_run_file(filename):
         return
 
-    for dir in dirs:
-        if dir in excluded_dir:
-            return
-
-    if key not in deps:
-        deps[key] = []
+    global parent
+    key = parent if parent != filename else filename
 
     with open(filename, "r") as file:
         hash = md5(file.read().encode())
@@ -68,23 +72,36 @@ def run_all_handler(frame: FrameType, event: str, _):
 def rerun_handler(frame: FrameType, event: str, _):
     if event != "call":
         return
-    # check all the hashes
-    with open("deps.json", "r") as file:
-        json_deps = json.load(file)
-    
-    print(json_deps)
-    # if any of the hashes are different, rerun the test
-    # if all the hashes are the same, don't rerun the test
-    # if the file is not in the deps, rerun the test
-    # if the file is in the deps, but the hash is different, rerun the test
-    # if the file is in the deps, and the hash is the same, don't rerun the test
-    # if the file is in the deps, but the file is not found, rerun the test
-    
+
+    filename = frame.f_code.co_filename
+    if not should_run_file(filename):
+        return
+
+    global parent, json_deps
+    key = parent if parent != filename else filename
+        
+    for key, value in json_deps.items():
+        for src, hash in enumerate(value):
+            deps[key].append(TestDependency(src, hash))
+            
+    with open(filename, "r") as file:
+        hash = md5(file.read().encode())
+        hashstr = hash.hexdigest()
+        for key in json_deps:
+            for dep in json_deps[key]:
+                if dep['src'] == filename and dep['hash'] == hashstr:
+                    return
+        newDeps[key].append(TestDependency(filename, hashstr))
 
 def pytest_runtest_call(item: pytest.Item):
-    global parent
+    global parent, json_deps
     isRunAll = item.config.getoption("--runAll")
-    logger.info(f"Running with runAll: {isRunAll}")
+    if not isRunAll:
+        with open("deps.json", "r") as file:
+            json_deps = json.load(file)
+            print("JSON DEPS", json_deps)
+    
+    logger.info(f"Running with isRunAll: {isRunAll}")
     parent = item.fspath.strpath
     trace_handler = run_all_handler if isRunAll else rerun_handler
     sys.settrace(trace_handler)
@@ -95,6 +112,10 @@ def pytest_runtest_teardown(item: pytest.Item):
 
 
 def pytest_sessionfinish(session, exitstatus):
+    isRunAll = session.config.getoption("--runAll")
+    if not isRunAll:
+        print("NEW DEPS", newDeps)
+    
     notUpdate = session.config.getoption("--noUpdate")
     if notUpdate:
         return
